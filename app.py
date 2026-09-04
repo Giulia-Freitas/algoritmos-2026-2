c8599a28d79be2e5bde3833f3994d76569172b9f
Giulia-Freitas <giuliadsfreitass@gmail.com>
2026-09-03 21:53:27 -0300

    Aula-09-03

---
+++ app.py
@@ -0,0 +1,14 @@
+print(type("Giulia")) # O tipo de dado é <class 'str'>
+print(type(26)) # O tipo de dado é <class 'int'>
+print(type(3.14)) # O tipo de dado é <class 'float'>
+print(type(3.1)) # O tipo de dado é <class 'float'>
+print(type(True)) # O tipo de dado é <class 'bool'>
+print(type(False)) # O tipo de dado é <class 'bool'>
+
+nome = "Giulia" # = Atribuição de valor a variável nome
+idade = 26 # = Atribuição de valor a variável idade
+print(nome) # = Exibe o valor da variável nome
+print(idade) # = Exibe o valor da variável idade
+
+print("Meu nome é", nome, "e minha idade é", idade) # = Exibe uma frase concatenando texto e variáveis
+print(f"Meu nome é {nome} e minha idade é {idade}") # = Exibe uma frase utilizando f-string para concatenar texto e variáveis
